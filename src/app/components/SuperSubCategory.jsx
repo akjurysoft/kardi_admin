8b9e01da443243cbbd7ef7e763f72af6ab378312
akjurysoft <akash.k@jurysoft.com>
2024-05-17 22:52:57 +0530

    fix brand info

--- src/app/components/SuperSubCategory.jsx
+++ src/app/components/SuperSubCategory.jsx
@@ -678,7 +678,7 @@ const SuperSubCategory = () => {
             <div className='flex flex-col space-y-2 mb-2'>
               <span className='text-[#344054] text-[14px] font-[500]'>Choose Category</span>
               {/* <input type='text' defaultValue={editData.sub_category_name} className='inputText' placeholder='Ex: Colour' name='edit_sub_category_name' onChange={getEditData} /> */}
-              <select defaultValue={editData.category_id} name='edit_category_id' onChange={getEditData}>
+              <select defaultValue={editData.category_id} name='edit_category_id' disabled onChange={getEditData}>
                 <option>Choose Category</option>
                 {categoryData && categoryData.map((e, i) =>
                   <option key={i} value={e.id}>{e.category_name}</option>
@@ -688,7 +688,7 @@ const SuperSubCategory = () => {
 
             <div className='flex flex-col space-y-2 mb-2'>
               <span className='text-[#344054] text-[14px] font-[500]'>Choose Sub Category</span>
-              <select defaultValue={editData.sub_category_id} name='edit_sub_category_id' onChange={getEditData}>
+              <select defaultValue={editData.sub_category_id} name='edit_sub_category_id' disabled onChange={getEditData}>
                 <option>Choose Sub Category</option>
                 {subCategoryData && subCategoryData.map((e, i) =>
                   <option key={i} value={e.id}>{e.sub_category_name}</option>
